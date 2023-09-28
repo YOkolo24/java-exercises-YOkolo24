772ce62f957dfd311323534c321c1131c1e90e11
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-09-28 18:40:03 +0100

    Updated and fully complete Client-Server Architecture exercises

--- exercises/client-server-architecture/src/main/java/com/cbfacademy/ExerciseServer.java
+++ exercises/client-server-architecture/src/main/java/com/cbfacademy/ExerciseServer.java
@@ -1,27 +1,28 @@
 package com.cbfacademy;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+public class ExerciseServer {
+    
+    public static void main(String[] args) {
+        
+        try (ServerSocket serverSocket = new ServerSocket(4040)) {
+            System.out.println("Server is listening on port 4040 ...");
 
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                    String message = in.readLine();
 
-public class ExerciseServer extends Exception {
-    
-    ServerSocket newServer = new ServerSocket(4040);
-    Socket newServerChannel = newServer.accept(); 
-    
-        try (InputStreamReader reader = new InputStreamReader(newServerChannel.getInputStream());
-            BufferedReader localHostReader = new BufferedReader(reader)) {
-                
-                String readText;
-                while ((readText = localHostReader.readLine()) != null) {
-                    System.out.println(readText);
-                } 
-        } catch (Exception e) {
-       System.out.println(e.getMessage());
-    }
-    
-       
+                    System.out.println("Received message from client: " + message);
+                }
+            }    
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }      
 }
