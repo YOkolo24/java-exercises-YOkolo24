c62f55f27c28055b5326828f2ad62e26d0e89a68
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-10-26 19:27:03 +0100

    Leap Year exercise - one test completed which currently fails

---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/LeapYear.java
@@ -0,0 +1,7 @@
+package com.cbfacademy;
+
+public class LeapYear {
+    public static boolean of(int value) {
+        throw new RuntimeException("Not implemented");
+    }
+}
---
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/LeapYearTests.java
@@ -0,0 +1,19 @@
+package com.cbfacademy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@DisplayName("The LeapYear class should")
+public class LeapYearTests {
+
+    @Test
+    @DisplayName("return true if year is divisible by 400")
+    public void returnTrueWhenInputIsDivisibleBy400() {
+        final boolean actual = LeapYear.of(400);
+        assertThat(actual, is(true));
+    }
+    
+}
+
