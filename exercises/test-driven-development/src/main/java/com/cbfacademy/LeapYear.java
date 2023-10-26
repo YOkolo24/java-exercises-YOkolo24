9a0dbd7d241882521318c00322f6754a4b5d5fcf
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-10-26 20:21:02 +0100

    Paramatized tests added to test divisible by 400

--- exercises/test-driven-development/src/main/java/com/cbfacademy/LeapYear.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/LeapYear.java
@@ -2,8 +2,8 @@ package com.cbfacademy;
 
 public class LeapYear {
 
-    public static Boolean isLeapYear(int value) {
-        if (value % 400 == 0) return true;
+    public static Boolean isLeapYear(int year) {
+        if (year % 400 == 0) return true;
 
         return false;
     }
--- exercises/test-driven-development/src/test/java/com/cbfacademy/LeapYearTests.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/LeapYearTests.java
@@ -1,19 +1,40 @@
 package com.cbfacademy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.util.stream.Stream;
+
 @DisplayName("The LeapYear class should")
 public class LeapYearTests {
-
-    @Test
-    @DisplayName("return true if year is divisible by 400")
-    public void returnTrueWhenInputIsDivisibleBy400() {
-        final boolean actual = LeapYear.isLeapYear(400);
-        assertThat(actual, is(true));
+    
+    @DisplayName("return true if year is divisible sby 400")
+    @ParameterizedTest
+    @MethodSource
+    
+    public void returnsTrueWhenYearIsDivisibleBy400(int year, Boolean expected) {
+        final Boolean result = LeapYear.isLeapYear(year);
+        assertThat(result, is(expected));
     }
     
+    static Stream<Arguments> returnsTrueWhenYearIsDivisibleBy400() {
+        return Stream.of(
+            arguments(1600, true),
+            arguments(1700, false),
+            arguments(1800, false),
+            arguments(1900, false),
+            arguments(2000, true),
+            arguments(2200, false),
+            arguments(2400, true)   
+        );
+    }
 }
 
