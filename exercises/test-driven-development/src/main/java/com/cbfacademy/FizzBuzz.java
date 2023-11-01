a8e8135c617793e1a8cbcf28b9544abc9ebbf1ac
Yvonne Okolo <yokolo@hotmail.co.uk>
2023-11-01 19:29:31 +0000

    FizzBuzz exercise, final solution

--- exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -1,10 +1,15 @@
 package com.cbfacademy;
 
 public class FizzBuzz {
-    public static String of(int value) {
+    
+    public static String of(Integer value) {
+
         if (value % 15 == 0) return "FizzBuzz";
+
         if (value % 3 == 0) return "Fizz";
+
         if (value % 5 == 0) return "Buzz";
-        return "1";
+
+        return value.toString();
     }
 }
--- exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTests.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/FizzBuzzTests.java
@@ -1,36 +1,42 @@
 package com.cbfacademy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 @DisplayName("The FizzBuzz class should")
 public class FizzBuzzTests {
 
-    @Test
-    @DisplayName("return 1 when the input is 1")
-    public void return1WhenInputIs1() {
-        final String actual = FizzBuzz.of(1);
-        assertThat(actual, is("1"));
+    @ParameterizedTest
+    @MethodSource("inputAndOutput")
+    @DisplayName("return expected value")
+    public void returnExpectedValue(Integer value, String expected) {
+        final String actual = FizzBuzz.of(value);
+        assertThat(actual, is(expected));
     }
-    @Test
-    @DisplayName("return 'Fizz' when the input is multiple of 3")
-    public void returnFizzWhenInputIsMultipleOf3() {
-        final String actual = FizzBuzz.of(3);
-        assertThat(actual, is("Fizz"));
-    } 
-    @Test
-    @DisplayName("return 'Buzz' when the input is multiple of 5")
-    public void returnBuzzWhenInputIsMultipleOf5() {
-        final String actual = FizzBuzz.of(5);
-        assertThat(actual, is("Buzz"));
-    }
-    @Test
-    @DisplayName("return 'FizzBuzz' when the input is multiple of 15")
-    public void returnFizzBuzzWhenInputIsMultipleOf15() {
-        final String actual = FizzBuzz.of(15);
-        assertThat(actual, is("FizzBuzz"));
+    static Stream<Arguments> inputAndOutput() {
+        return Stream.of(
+            arguments(1, "1"),
+            arguments(2, "2"),
+            arguments(3, "Fizz"),
+            arguments(4, "4"),
+            arguments(5, "Buzz"),
+            arguments(6, "Fizz"),
+            arguments(7, "7"),
+            arguments(8, "8"),
+            arguments(9, "Fizz"),
+            arguments(10, "Buzz"),
+            arguments(11, "11"),
+            arguments(12, "Fizz"),
+            arguments(13, "13"),
+            arguments(14, "14"),
+            arguments(15, "FizzBuzz")
+        );
     }
 }
